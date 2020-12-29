94ea95fafd5265af9e0903f1ad4c592af281f7f8
fanju <fanju@DESKTOP-KM92V2K>
2020-12-29 16:37:28 +0100

    prueba en main

--- src/main/java/packCodigo/Main.java
+++ src/main/java/packCodigo/Main.java
@@ -4,6 +4,7 @@ import packVentanas.VLogin;
 
 public class Main {
 
+	// comentario de prueba
 	public static void main(String[] args) 
 			throws NoArchivoAudioException {
 		VLogin vL = new VLogin();
