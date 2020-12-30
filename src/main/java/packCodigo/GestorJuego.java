d51e7885be4ac3783006143f23de059fe387e418
jonro <jonro1921@gmail.com>
2020-12-30 17:25:11 +0100

    version 1.3

---
+++ src/main/java/packCodigo/GestorJuego.java
@@ -0,0 +1,25 @@
+package packCodigo;
+
+public class GestorJuego {
+
+	private static GestorJuego miGestorJuego;
+	private Partida partida;
+	
+	private GestorJuego() {}
+	
+	public static GestorJuego getGestorJuego() {
+		if (miGestorJuego == null) {
+			miGestorJuego = new GestorJuego();
+		}
+		return miGestorJuego;
+	}
+	
+	public Partida getPartida() {
+		return partida;
+	}
+	
+	public void comenzarPartida() {
+		partida = new Partida();
+	}
+	
+}
