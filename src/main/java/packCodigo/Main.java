5873ac78fbf556bfbc5964e040dd05f83d468a38
fanju <fanju@DESKTOP-KM92V2K>
2020-12-31 15:36:28 +0100

    quitada la prueba inicial

--- src/main/java/packCodigo/Main.java
+++ src/main/java/packCodigo/Main.java
@@ -3,9 +3,6 @@ package packCodigo;
 import packVentanas.IU_Login;
 
 public class Main {
-
-	// comentario de prueba
-	// prueba 2
 	public static void main(String[] args) 
 			throws NoArchivoAudioException {
 		IU_Login login = new IU_Login();
