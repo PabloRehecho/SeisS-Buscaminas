f5ebf6cde17c2078fec6aab4704a09970fa1e197
unai <unai13hego@gmail.com>
2020-12-31 17:47:19 +0100

    Premios V3

--- src/main/java/packVentanas/VError.java
+++ src/main/java/packVentanas/VError.java
@@ -1,79 +1,4 @@
-<<<<<<< HEAD
 package packVentanas;
-
-import java.awt.BorderLayout;
-import java.awt.FlowLayout;
-
-import javax.swing.JButton;
-import javax.swing.JDialog;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-import javax.swing.GroupLayout;
-import javax.swing.GroupLayout.Alignment;
-import javax.swing.JLabel;
-import javax.swing.SwingConstants;
-
-public class VError extends JDialog {
-
-	private final JPanel contentPanel = new JPanel();
-	private JButton okButton;
-
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		try {
-			VError dialog = new VError();
-			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
-			dialog.setVisible(true);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-
-	/**
-	 * Create the dialog.
-	 */
-	public VError() {
-		setBounds(100, 100, 450, 300);
-		getContentPane().setLayout(new BorderLayout());
-		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
-		getContentPane().add(contentPanel, BorderLayout.CENTER);
-		contentPanel.setLayout(null);
-		
-		JLabel lblError = new JLabel("New label");
-		lblError.setBounds(196, 74, 46, 14);
-		lblError.setHorizontalAlignment(SwingConstants.CENTER);
-		contentPanel.add(lblError);
-		{
-			JPanel buttonPane = new JPanel();
-			getContentPane().add(buttonPane, BorderLayout.SOUTH);
-			{
-				okButton = new JButton("OK");
-				okButton.setActionCommand("OK");
-				getRootPane().setDefaultButton(okButton);
-			}
-			GroupLayout gl_buttonPane = new GroupLayout(buttonPane);
-			gl_buttonPane.setHorizontalGroup(
-				gl_buttonPane.createParallelGroup(Alignment.LEADING)
-					.addGroup(gl_buttonPane.createSequentialGroup()
-						.addGap(194)
-						.addComponent(okButton, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-						.addGap(193))
-			);
-			gl_buttonPane.setVerticalGroup(
-				gl_buttonPane.createParallelGroup(Alignment.LEADING)
-					.addGroup(gl_buttonPane.createSequentialGroup()
-						.addComponent(okButton)
-						.addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-			);
-			buttonPane.setLayout(gl_buttonPane);
-		}
-	}
-}
-=======
-package packVentanas;
-
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
 
@@ -88,16 +13,13 @@ import javax.swing.SwingConstants;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
+
 public class VError extends JDialog {
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 	private final JPanel contentPanel = new JPanel();
 	private JButton okButton;
 	private JLabel lblError;
-
 	/**
 	 * Launch the application.
 	 */
@@ -161,4 +83,4 @@ public class VError extends JDialog {
 		lblError.setText(pError);
 	}
 }
->>>>>>> branch 'master' of https://github.com/PabloRehecho/SeisS-Buscaminas.git
+
