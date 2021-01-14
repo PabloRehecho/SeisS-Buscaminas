83ef17a10cb54e3e663470ae8638e1922b029926
Mikel <Mikel@DESKTOP-1S3DMC7>
2021-01-14 17:13:56 +0100

    s

---
+++ src/main/java/packCodigo/CasillaMina50.java
@@ -0,0 +1,21 @@
+package packCodigo;
+
+public class CasillaMina50 extends Casilla {
+
+	public CasillaMina50() {
+		super();
+	}
+
+	public void descubrir() {
+		super.descubrir();
+	}
+
+	public void inicializar(String coor) {
+		super.inicializar(coor);
+	}
+
+	public void cambioBandera() {
+		super.cambioBandera();
+	}
+
+}
