09beb75803fb8fbe2d494bba405a16f7ff1a087c
Mikel <Mikel@DESKTOP-1S3DMC7>
2021-01-14 17:13:41 +0100

    asd

--- src/main/java/packCodigo/CasillaFactory.java
+++ src/main/java/packCodigo/CasillaFactory.java
@@ -2,30 +2,31 @@ package packCodigo;
 
 public class CasillaFactory {
 
-	
 	private static CasillaFactory miCasillaFactory = new CasillaFactory();
-	
-	private CasillaFactory(){
-		
+
+	private CasillaFactory() {
+
 	}
-	
-	public static CasillaFactory getMiFactoria(){
+
+	public static CasillaFactory getMiFactoria() {
 		return miCasillaFactory;
 	}
-	
-	public Casilla generarCasilla(String tipo){	
+
+	public Casilla generarCasilla(String tipo) {
 		Casilla cas = null;
-		
-		if(tipo == "Mina"){
+
+		if (tipo == "Mina") {
 			cas = new CasillaMina();
-		} else if (tipo == "Numero"){
+		} else if (tipo == "Numero") {
 			cas = new CasillaNumero();
-		} else if (tipo == "Vacia"){
+		} else if (tipo == "Vacia") {
 			cas = new CasillaVacia();
+		} else if (tipo == "MinaReset") {
+			cas = new CasillaMinaReset();
+		} else if (tipo == "Mina50") {
+			cas = new CasillaMina50();
 		}
 		return cas;
 	}
-	
-	
-}
 
+}
