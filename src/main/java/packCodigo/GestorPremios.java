5e7a84bb8ad4328313144b15e347d22038e3c23e
unai <unai13hego@gmail.com>
2021-01-14 17:03:16 +0100

    A Gestor Premios

--- src/main/java/packCodigo/GestorPremios.java
+++ src/main/java/packCodigo/GestorPremios.java
@@ -1,6 +1,9 @@
 package packCodigo;
 
 import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.HashSet;
+import java.util.Set;
 
 public class GestorPremios {
 	private static GestorPremios miGestorPremios;
@@ -31,5 +34,72 @@ public class GestorPremios {
 	}
 	public void ganarPremio(String pEmail, String pNombre) {
 		GestorBD.getGestorBD().execSQL2("INSERT INTO UsuarioPremio (emailJugador, nombrePremio) values ('"+pEmail+"', '"+pNombre+"')");
+	}
+	public boolean comprobarPremio(int[] hitos) {
+		Set<String> nMios=new HashSet<String>();
+		int i=0;
+		int cond[];
+		String nom[];
+		String descr[];
+		boolean nuevo=false;
+		
+		String email=GestorUsuario.getGestorUsuario().getUsuario();
+		ResultSet todos, actuales;
+		todos=getTodosPremios();
+		actuales=getNombrePremios(email);
+		cond=new int[20];
+		descr=new String[20];
+		nom=new String[20];
+		try {
+			while(todos.next()) {
+				cond[i]=todos.getInt("requisito");
+				nom[i]=todos.getString("nombre");
+				descr[i]=todos.getString("descripcion");
+				i++;
+			}
+			todos.close();
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}
+		try {
+			while(actuales.next()) {
+				nMios.add(todos.getString("Nombre"));
+			}
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}
+		int in=0;
+		while(in<=i) {
+			if(!nMios.isEmpty()) {
+				if(!nMios.contains(nom[i])) {
+					if(descr[i]=="Ganar en el nivel 1") {
+						if(hitos[0]>=cond[i]){
+							nuevo=true;
+							ganarPremio(email, nom[i]);
+						}
+					}
+				}
+				else if(descr[i]=="Ganar en el nivel 2") {
+					if(hitos[1]>=cond[i]){
+						nuevo=true;
+					}
+				}
+				else if(descr[i]=="Ganar en el nivel 3") {
+					if(hitos[2]>=cond[i]){
+						nuevo=true;
+					}
+				}
+				else {
+					if(hitos[3]>=cond[i]){
+						nuevo=true;
+					}
+				}
+				if(nuevo) {
+					ganarPremio(email, nom[i]);
+				}
+			}
+			in++;
+		}
+		return nuevo;
 	}
 }
