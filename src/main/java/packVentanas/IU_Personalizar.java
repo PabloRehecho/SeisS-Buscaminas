301a13a06c49904c7bd1b54ba86af87a1f951933
Mourouh <Jonmoro2000@gmail.com>
2021-01-15 16:03:41 +0100

    No reirse de los subnormales, por favor

--- src/main/java/packVentanas/IU_Personalizar.java
+++ src/main/java/packVentanas/IU_Personalizar.java
@@ -85,60 +85,15 @@ public class IU_Personalizar extends JFrame {
 		panel.setLayout(null);	
 		
 		ResultSet rs= Buscaminas.getBuscaminas().extraerPersonalizacion();
-		System.out.println(rs);
 		try {
 			rs.next();
 			personalizacion[0]=rs.getInt("imagenMinas");
 			personalizacion[1]=rs.getInt("imagenCara");
 			personalizacion[2]=rs.getInt("sonido");
-			System.out.println(personalizacion[1]);
 			rs.close();
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-		/*ResultSet rsm = Buscaminas.getBuscaminas().conseguirPathsMinas(personalizacion[0]);
-		try {
-			rsm.next();
-			pathMinas[0]=rsm.getString("direccion");
-			System.out.println(pathMinas[0]);
-			rsm.next();
-			pathMinas[1]=rsm.getString("direccion");
-			System.out.println(pathMinas[1]);
-			rsm.next();
-			pathMinas[2]=rsm.getString("direccion");
-			System.out.println(pathMinas[2]);
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}*/
-		/*JLabel lblMinas1 = new JLabel("Minas nivel 1: " + valores[7]);
-		lblMinas1.setBounds(10, 29, 119, 14);
-		panel.add(lblMinas1);
-		
-		JLabel lblMinas2 = new JLabel("Minas nivel 2: " + valores[8]);
-		lblMinas2.setBounds(10, 54, 119, 14);
-		panel.add(lblMinas2);
-		
-		JLabel lblMinas3 = new JLabel("Minas nivel 3: " + valores[9]);
-		lblMinas3.setBounds(10, 79, 119, 14);
-		panel.add(lblMinas3);
-		
-		JLabel lblTamano1 = new JLabel("Tamano nivel 1: " + valores[3] + " x " + valores[0]);
-		lblTamano1.setBounds(10, 104, 162, 14);
-		panel.add(lblTamano1);
-		
-		JLabel lblTamano2 = new JLabel("Tamano nivel 2: " + valores[4] + " x " + valores[1]);
-		lblTamano2.setBounds(10, 129, 162, 14);
-		panel.add(lblTamano2);
-		
-		JLabel lblTamano3 = new JLabel("Tamano nivel 3: " + valores[5] + " x " +  valores[2]);
-		lblTamano3.setBounds(10, 154, 162, 14);
-		panel.add(lblTamano3);
-		
-		JLabel lblMensajeDeAyuda = new JLabel("Mensaje de ayuda: " + valores[6]);
-		lblMensajeDeAyuda.setBounds(10, 179, 162, 14);
-		panel.add(lblMensajeDeAyuda);*/
-		
-		
 		
 		choiceMina = new Choice();
 		choiceMina.setBounds(10, 30, 86, 20);
@@ -171,8 +126,8 @@ public class IU_Personalizar extends JFrame {
 		
 		JLabel imagenCara = new JLabel();
 		imagenCara.setBounds(150, 101, 40, 40);
-		imagenCara.setIcon(new ImageIcon(VBuscaminas.class.getResource("/Reset" + personalizacion[0] + ".png")));
-		panel.add(imagenMina);
+		imagenCara.setIcon(new ImageIcon(VBuscaminas.class.getResource("/Reset" + personalizacion[1] + ".png")));
+		panel.add(imagenCara);
 		
 		JLabel imagenCara1 = new JLabel();
 		imagenCara1.setBounds(250, 101, 40, 40);
@@ -216,62 +171,6 @@ public class IU_Personalizar extends JFrame {
 		}
 		choiceSonido.select(personalizacion[2]-1);
 		panel.add(choiceSonido);
-		
-		/*choiceMinas3 = new Choice();
-		choiceMinas3.setBounds(139, 76, 86, 20);
-		for(int i=30; i<=99; i++){
-			choiceMinas3.add("" + i +"");
-		}
-		panel.add(choiceMinas3);
-		
-		choiceTamano1a = new Choice();
-		choiceTamano1a.setBounds(182, 101, 86, 20);
-		for(int i=1; i<=10; i++){
-			choiceTamano1a.add("" + i +"");
-		}
-		panel.add(choiceTamano1a);
-		
-		choiceTamano1b = new Choice();
-		choiceTamano1b.setBounds(278, 101, 86, 20);
-		for(int i=1; i<=15; i++){
-			choiceTamano1b.add("" + i +"");
-		}
-		panel.add(choiceTamano1b);
-		
-		choiceTamano2a = new Choice();
-		choiceTamano2a.setBounds(182, 126, 86, 20);
-		for(int i=5; i<=20; i++){
-			choiceTamano2a.add("" + i +"");
-		}
-		panel.add(choiceTamano2a);
-		
-		choiceTamano2b = new Choice();
-		choiceTamano2b.setBounds(278, 126, 86, 20);
-		for(int i=10; i<=30; i++){
-			choiceTamano2b.add("" + i +"");
-		}
-		panel.add(choiceTamano2b);
-		
-		choiceTamano3a = new Choice();
-		choiceTamano3a.setBounds(182, 151, 86, 20);
-		for(int i=15; i<=30; i++){
-			choiceTamano3a.add("" + i +"");
-		}
-		panel.add(choiceTamano3a);
-		
-		choiceTamano3b = new Choice();
-		choiceTamano3b.setBounds(278, 151, 86, 20);
-		for(int i=15; i<=50; i++){
-			choiceTamano3b.add("" + i +"");
-		}
-		panel.add(choiceTamano3b);
-		
-		choiceMensaje = new Choice();
-		choiceMensaje.setBounds(235, 176, 86, 20);
-		for(int i=1; i<=5; i++){
-			choiceMensaje.add("" + i +"");
-		}
-		panel.add(choiceMensaje);*/
 		
 		JButton btnVolverAlMenu = new JButton("Volver al menu");
 		btnVolverAlMenu.setBackground(Color.RED);
